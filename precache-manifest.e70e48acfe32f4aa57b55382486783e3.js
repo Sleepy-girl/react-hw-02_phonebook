8721faed37cd2d3a7c9317ed3a03ceb1292b76de
nyuta <pillowforneedles@gmail.com>
2020-08-15 11:46:09 +0300

    Updates

---
+++ precache-manifest.e70e48acfe32f4aa57b55382486783e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e41f318c2c1feb03a1b2618b2b31fc66",
+    "url": "/react-hw-02_phonebook/index.html"
+  },
+  {
+    "revision": "9f49a23bc588025f9400",
+    "url": "/react-hw-02_phonebook/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f18a4ea95b083e03c5b9",
+    "url": "/react-hw-02_phonebook/static/js/2.e7518a20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-02_phonebook/static/js/2.e7518a20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f49a23bc588025f9400",
+    "url": "/react-hw-02_phonebook/static/js/main.95f1f713.chunk.js"
+  },
+  {
+    "revision": "118312f725485d8a6a92",
+    "url": "/react-hw-02_phonebook/static/js/runtime-main.814de11f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-hw-02_phonebook/static/media/logo.5d5d9eef.svg"
+  }
+]);
